1b0e03cb5cc543ee6c3d87e8a2c2e40370dfb7fd
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-19 14:11:54 +0900

    SampleAnalogSlide

--- Arduino/SampleAnalogSlide/SampleAnalogSlide.ino
+++ Arduino/SampleAnalogSlide/SampleAnalogSlide.ino
@@ -2,12 +2,18 @@
 // https://blog.naver.com/ljy9378/221357120126
 // Sample source cod for Arduino
 
+// 아날로그 슬라이드 모듈
+// 아날로그 A0 핀 사용
+
 void setup() {
+  // 시리얼 모니터 9600 사용
   Serial.begin(9600);  
 }
 
 void loop() {
+  // 아날로그 A0으로 받은 데이터를 변수에 저장
   int cur = analogRead(A0);
+  // 시리얼 모니터에 센서 값 출력
   Serial.print(cur);
   delay(100);
 }
--- Arduino/SampleBlutoothIR/SampleBlutoothIR.ino
+++ Arduino/SampleBlutoothIR/SampleBlutoothIR.ino
@@ -14,15 +14,28 @@ void setup() {
 void loop() {
   if (btSerial.available()){
     char bt = btSerial.read();
-    if(bt == 'a') {
-      // Turn on/off
+    // Turn on/off
+    if(bt == 'p') {
       irsend.sendNEC(0x20DF10EF, 32);
-      delay(100);
+      delay(500);
       irsend.sendNEC(0x122430CF, 32);
     }
-    // volume up
-    if(bt == 'b') irsend.sendNEC(0x1224C837, 32);
-    // volume down
-    if(bt == 'c') irsend.sendNEC(0x122428D7, 32);
+    // volume up/down
+    else if(bt == 'v') irsend.sendNEC(0x1224C837, 32);
+    else if(bt == 'w') irsend.sendNEC(0x122428D7, 32);
+    // channel up/down
+    else if(bt == 'c') irsend.sendNEC(0x1224E817, 32);
+    else if(bt == 'd') irsend.sendNEC(0x122418E7, 32);
+    // channel
+    else if(bt == '0') irsend.sendNEC(0x122400FF, 32);
+    else if(bt == '1') irsend.sendNEC(0x1224807F, 32);
+    else if(bt == '2') irsend.sendNEC(0x122440BF, 32);
+    else if(bt == '3') irsend.sendNEC(0x1224C03F, 32);
+    else if(bt == '4') irsend.sendNEC(0x122420DF, 32);
+    else if(bt == '5') irsend.sendNEC(0x1224A05F, 32);
+    else if(bt == '6') irsend.sendNEC(0x1224609F, 32);
+    else if(bt == '7') irsend.sendNEC(0x1224A659, 32);
+    else if(bt == '8') irsend.sendNEC(0x122400FF, 32);
+    else if(bt == '9') irsend.sendNEC(0x1224906F, 32);
   }
 }
