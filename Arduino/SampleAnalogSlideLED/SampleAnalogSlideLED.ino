33fe35cee600e15e232e55ec936818729575c3c0
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-20 00:44:20 +0900

    SampleAnalogSlideLED

--- Arduino/SampleAnalogSlideLED/SampleAnalogSlideLED.ino
+++ Arduino/SampleAnalogSlideLED/SampleAnalogSlideLED.ino
@@ -5,9 +5,12 @@
 void setup() {}
 
 void loop() {
-  int cur = analogRead(A0);
-  float R = 255 - (float)cur/1023*255;
-  analogWrite(9, R);
-  analogWrite(10, 255);
-  analogWrite(11, 255);
+	// 아날로그 핀 A0에 입력받은 센서값을 변수에 저장
+	int cur = analogRead(A0);
+	// 센서값이 0~1023이므로 RGB값에 맞게 범위 조정(0~255)
+	float R = 255 - (float)cur/1023*255;
+	// RGB핀은 각각 9,10,11
+	analogWrite(9, R);
+	analogWrite(10, 255);
+	analogWrite(11, 255);
 }
