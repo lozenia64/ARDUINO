27abc79507b33d855da1e8f780fa9a23fdf4c0e2
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-22 00:15:42 +0900

    SampleServoMotor

---
+++ Arduino/SampleServoMotor/SampleServoMotor.ino
@@ -0,0 +1,24 @@
+// lozenia64
+// https://blog.naver.com/ljy9378/221357120126
+// Sample source cod for Arduino
+
+#include <Servo.h> 
+Servo servo;
+int servoPin = 9;
+void setup() { 
+    servo.attach(servoPin); 
+} 
+
+void loop() { 
+  // scan from 0 to 180 degrees
+  for(int angle = 0; angle < 180; angle++) { 
+    servo.write(angle); 
+    delay(5); 
+  } 
+  // now scan back from 180 to 0 degrees
+  for(int angle = 180; angle > 0; angle--) { 
+    servo.write(angle); 
+    delay(5); 
+  } 
+} 
+
