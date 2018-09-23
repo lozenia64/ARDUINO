ad20373992c02167ac42594e90ea24a996231420
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-23 15:37:07 +0900

    SampleBlutoothLED

--- Arduino/SampleBlutoothLED/SampleBlutoothLED.ino
+++ Arduino/SampleBlutoothLED/SampleBlutoothLED.ino
@@ -2,6 +2,9 @@
 // https://blog.naver.com/ljy9378/221357120126
 // Sample source cod for Arduino
 
+// 블루투스 모듈 사용
+// 블루투스 모듈 디지털 4,5번 핀 사용
+
 #include <SoftwareSerial.h>
 SoftwareSerial btSerial(5,4);
 void setup() {
@@ -11,6 +14,7 @@ void setup() {
 void loop() {
   if (btSerial.available()){
     char bt = btSerial.read();
+	//블루투스로 'a'를 받으면 LED on, 'b'를 받으면 LED off
     if(bt == 'a') digitalWrite(13,HIGH);
     if(bt == 'b') digitalWrite(13,LOW);
   }
