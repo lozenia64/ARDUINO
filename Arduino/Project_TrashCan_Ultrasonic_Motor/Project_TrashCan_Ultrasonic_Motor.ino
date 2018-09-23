3bd82128cec7b0706535b8f36f1a41ded5e59552
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-23 15:35:06 +0900

    Project_TrashCan_Ultrasonic_Motor

--- Arduino/Project_TrashCan_Ultrasonic_Motor/Project_TrashCan_Ultrasonic_Motor.ino
+++ Arduino/Project_TrashCan_Ultrasonic_Motor/Project_TrashCan_Ultrasonic_Motor.ino
@@ -1,8 +1,14 @@
+// lozenia64
+// https://blog.naver.com/ljy9378/221357120126
+// Sample source cod for Arduino
+
+// 서보모터, 초음파센서 사용
+// 서보모터 디지털 10, 11번 핀 사용
+// 초음파센서 디지털 9번 핀 사용
+
 #include <Servo.h> 
 Servo servo; 
-int echoPin = 10;
-int trigPin = 11;
-int servoPin = 9;
+int echoPin = 10, trigPin = 11, servoPin = 9;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -19,9 +25,11 @@ void loop() {
   digitalWrite(trigPin, LOW);
 
   unsigned long duration = pulseIn(echoPin, HIGH); 
+  //초음파센서가 읽은 거리, cm로 표현할 변수 distance
   float distance = ((float)(340 * duration) / 10000) / 2;  
   servo.write(0);
 
+  //거리가 20cm 이내일때 모터 작동
   if (distance < 20){
     servo.write(110); 
     delay(3000);
