24b645d3d543c007f0b57bc0122127f569616ff8
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-24 15:41:54 +0900

    SampleDigitalSlide

--- Arduino/SampleDigitalSlide/SampleDigitalSlide.ino
+++ Arduino/SampleDigitalSlide/SampleDigitalSlide.ino
@@ -2,13 +2,19 @@
 // https://blog.naver.com/ljy9378/221357120126
 // Sample source cod for Arduino
 
+// 디지털슬라이드 모듈 사용
+// 디지털 12번 핀 사용
+
 void setup() {
+  //디지털 12번핀에 입력, 13번 핀 LED 테스트
   pinMode(12, INPUT);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
+  //입력받은 센서값을 저장할 변수 button
   int button = digitalRead(12);
+  //button은 0,1 둘 중 하나의 값을 가짐
   if(button == HIGH) digitalWrite(13, HIGH);
   else if(button == LOW) digitalWrite(13, LOW);
 }
