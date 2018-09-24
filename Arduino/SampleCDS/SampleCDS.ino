c7df54953507e139180e9a56e652aec952e086ed
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-24 15:35:14 +0900

    SampleCDS

--- Arduino/SampleCDS/SampleCDS.ino
+++ Arduino/SampleCDS/SampleCDS.ino
@@ -2,11 +2,16 @@
 // https://blog.naver.com/ljy9378/221357120126
 // Sample source cod for Arduino
 
+// 조도센서 사용
+// 조도센서 아날로그 핀 A0 사용
+
 void setup() {
- Serial.begin(9600);
+  //시리얼모니터 9600번 사용
+  Serial.begin(9600);
 }
 
 void loop() {
+  //아날로그 0번 핀에서 받은 센서 값을 저장할 변수 cur
   int cur = analogRead(A0);
   Serial.println(cur);
   delay(1000);
